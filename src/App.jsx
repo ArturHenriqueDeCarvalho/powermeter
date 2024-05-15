540db52d6ab75330ceb433e53db968975cb48f9a
tmz81 <tmz_81@hotmail.com>
2024-05-15 17:07:02 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import SectionFour from "./components/SectionFour";
 import NavBar from "./components/NavBar";
 import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo"
 import SectionThree from "./components/SectionThree";
-import SectionTwo from "./components/SectionTwo";
 
 function App() {
   return (
-    <>
+    <div>
       <NavBar />
       <SectionOne />
       <SectionTwo />
       <SectionThree />
-    </>
+      <SectionFour />
+    </div>
   );
 }
 
