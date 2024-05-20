0f958c21fb251a2fdb4381f8935ce5c2e72e47d0
Artur Henrique de Carvalho <arturcarvallho@hotmai.com>
2024-05-20 14:37:05 -0300

    Commit de alterações de cores do site.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -135,7 +135,7 @@ export default function NavBar() {
           </Box>
 
           <Button
-            href="#contato"
+            href="#contat"
             variant="contained"
             sx={{
               display: { xs: "none", md: "flex" },
